0b467924d59c2c2d0ff631cfb8bb7614589feb4b
Theodore Carter <theocarter819@gmail.com>
2024-09-01 19:36:25 -0400

    Changed a little something

--- src/main/java/ver1/HelloWorld.java
+++ src/main/java/ver1/HelloWorld.java
@@ -3,6 +3,7 @@ package ver1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Eclipse");
+        System.out.println("TJ is working locally on his branch");
     }
 }
 
