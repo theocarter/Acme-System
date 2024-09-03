3de6fdf6f3d8a0090d468b26733171664259bb4a
Theodore Carter <theocarter819@gmail.com>
2024-09-02 20:55:11 -0400

    Added a something.

--- src/main/java/ver1/HelloWorld.java
+++ src/main/java/ver1/HelloWorld.java
@@ -1,6 +1,12 @@
 package ver1;
 
 public class HelloWorld {
+
+    public static void heyNow() {
+        String Msg = "Hey Now!";
+        System.out.println(Msg);
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello from Eclipse");
         System.out.println("TJ is working locally on his branch");
